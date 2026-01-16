d9c8328a1da1c70833131abcc0c22e89163fd435
Pavel Shukhman <pavel@reliza.io>
2026-01-16 07:49:16 -0500

    fix: add use forwarded headers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    KnownNetworks = { },
+    KnownProxies = { }
+});
 
 app.MapControllers();
 
