c385a6fc12e014817480d8869d80b3cfe8a33617
Pavel Shukhman <pavel@reliza.io>
2026-01-16 07:52:52 -0500

    chore: add middlewares

--- Program.cs
+++ Program.cs
@@ -1,8 +1,84 @@
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(options =>
+{
+    options.EnableEndpointRateLimiting = true;
+    options.StackBlockedRequests = false;
+    options.RealIpHeader = "X-Forwarded-For";
+    options.ClientIdHeader = "X-ClientId";
+    options.HttpStatusCode = StatusCodes.Status429TooManyRequests;
+    options.GeneralRules =
+    [
+        new RateLimitRule
+        {
+            Endpoint = "*",
+            Period = "1m",
+            Limit = 60
+        }
+    ];
+});
+builder.Services.Configure<IpRateLimitPolicies>(options =>
+{
+    options.IpRules =
+    [
+        new IpRateLimitPolicy
+        {
+            Ip = "*",
+            Rules =
+            [
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Period = "1m",
+                    Limit = 60
+                }
+            ]
+        }
+    ];
+});
+builder.Services.Configure<ClientRateLimitOptions>(options =>
+{
+    options.EnableEndpointRateLimiting = true;
+    options.StackBlockedRequests = false;
+    options.HttpStatusCode = StatusCodes.Status429TooManyRequests;
+    options.GeneralRules =
+    [
+        new RateLimitRule
+        {
+            Endpoint = "*",
+            Period = "1m",
+            Limit = 120
+        }
+    ];
+});
+builder.Services.Configure<ClientRateLimitPolicies>(options =>
+{
+    options.ClientRules =
+    [
+        new ClientRateLimitPolicy
+        {
+            ClientId = "*",
+            Rules =
+            [
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Period = "1m",
+                    Limit = 120
+                }
+            ]
+        }
+    ];
+});
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
 var app = builder.Build();
 
@@ -12,6 +88,12 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
     KnownNetworks = { },
     KnownProxies = { }
 });
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseIpRateLimiting();
+app.UseClientRateLimiting();
 
 app.MapControllers();
 
